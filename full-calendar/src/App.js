170a7ad00154ff03aa9184bd34b1173da35914ca
Ashlynn223 <50315144+Ashlynn223@users.noreply.github.com>
2022-10-03 22:50:44 -0700

    make calander interactive so events can added

--- full-calendar/src/App.js
+++ full-calendar/src/App.js
@@ -1,17 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import interactionPlugin from "@fullcalendar/interaction";
 
 function App() {
+  const EventItem = ({ info }) => {
+    const { event } = info;
+    return (
+      <div>
+        <p>{event.title}</p>
+      </div>
+    );
+  };
+  const [events, setEvents] = useState([
+    {
+      start: new Date(),
+      end: new Date(),
+      title: "Unicorn",
+      id: "uuid()",
+    }
+  ]);
+
+  const handleSelect = (info) => {
+    const { start, end } = info;
+    const eventNamePrompt = prompt("Enter, event name");
+    if (eventNamePrompt) {
+      setEvents([
+        ...events,
+        {
+          start,
+          end,
+          title: eventNamePrompt,
+          id: "uuid()",
+        },
+      ]);
+    }
+  };
   return (
     <div className="App">
       <FullCalendar
+      editable
         headerToolbar={{
           start: "today prev next",
           center: "title",
           end: "dayGridMonth dayGridWeek dayGridDay",
         }}
-        plugins={[dayGridPlugin]}
+        events={events}
+        eventContent={(info) => <EventItem info={info} />}
+        selectable
+        select={handleSelect}
+        plugins={[dayGridPlugin, interactionPlugin]}
         views={["dayGridMonth", "dayGridWeek", "dayGridDay"]}
       />
     </div>
