419cf271bdd6f3dca8f82bb237f3d7fdc5937aeb
Jake-Mitchell <39348241+Jake-Mitchell@users.noreply.github.com>
2022-10-08 18:04:26 -0700

    adds date and time inputs for calendar, not yet hooked up to UI for event creation

--- big-calendar/src/App.js
+++ big-calendar/src/App.js
@@ -1,23 +1,37 @@
-import { Calendar, momentLocalizer } from 'react-big-calendar'
-import moment from 'moment'
-import './App.css';
-import "react-big-calendar/lib/css/react-big-calendar.css"
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "./App.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useState } from "react";
 
-const localizer = momentLocalizer(moment)
-
-// title?: React.ReactNode | undefined;
-//     start?: Date | undefined;
-//     end?: Date | undefined;
+const localizer = momentLocalizer(moment);
 
 const events = [
   {
     title: "Beetha's Birthday",
     start: new Date(),
-    end: new Date()
-  }
-]
+    end: new Date(),
+  },
+];
+
+function padTo2Digits(num) {
+  return num.toString().padStart(2, "0");
+}
+
+function formatDate(date) {
+  return [
+    date.getFullYear(),
+    padTo2Digits(date.getMonth() + 1),
+    padTo2Digits(date.getDate()),
+  ].join("-");
+}
 
 function App() {
+  const [startDate, setStartDate] = useState(formatDate(new Date())); // may set this to a Date object later.
+  const [endDate, setEndDate] = useState(formatDate(new Date()));
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+
   return (
     <div className="App">
       <div>
@@ -29,6 +43,30 @@ function App() {
           style={{ height: 500 }}
         />
       </div>
+      <label>Start Date</label>
+      <input
+        type="date"
+        onChange={(e) => {
+          setStartDate(e.target.value);
+          setEndDate(e.target.value);
+        }}
+      />
+      <label>Start Time</label>
+      <input
+        type="time"
+        onChange={(e) => {
+          setStartTime(e.target.value);
+        }}
+      />
+      <label>End Date</label>
+      <input type="date" />
+      <label>End Time</label>
+      <input
+        type="time"
+        onChange={(e) => {
+          setEndTime(e.target.value);
+        }}
+      />
     </div>
   );
 }
