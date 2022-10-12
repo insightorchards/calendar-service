a19ded1b753492b1a04c5805d10e2ceedecde86d
Ellie Bahadori <elliedori@gmail.com>
2022-10-11 22:07:56 -0700

    Build out dummy endpoint in Express

---
+++ api/controllers/calendarEntry.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from 'express';
+
+interface CalendarEntry {
+    id: string
+    eventId: string
+    creatorId: string
+    title: string
+    description: string
+    isAllDay: boolean
+    startTimeUtc: Date
+    endTimeUtc: Date
+    createdAt: Date
+    updatedAt: Date
+  };
+
+  export const getCalendarEntries = (request: Request, response: Response, next: NextFunction) => {
+    const date = new Date()
+    const entries: CalendarEntry[] = [
+      {
+        id: "id",
+        eventId: "eventId",
+        creatorId: "creatorId",
+        title: "title",
+        description: "description",
+        isAllDay: false,
+        startTimeUtc: date,
+        endTimeUtc: date,
+        createdAt: date,
+        updatedAt: date,
+      },
+    ];
+
+    response.status(200).json(entries);
+  };
