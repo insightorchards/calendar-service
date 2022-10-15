d6391473e10e2c4c916eb5cd75653d62154322f0
sofiapaganin <paganin@berkeley.edu>
2022-10-15 16:17:34 -0700

    get entry from mongodb

--- api/app.ts
+++ api/app.ts
@@ -5,11 +5,20 @@ import { connectToServer, getDb } from "./db/connect";
 const app = express();
 const port = 4000;
 
+let database;
+connectToServer(async () => {
+  database = getDb();
+  const entries = await database
+    .collection("calendarEntries")
+    .find({})
+    .limit(5)
+    .toArray();
+
+  console.log(JSON.stringify(entries));
+});
+
 app.listen(port, () => {
   console.log(`Calendar application is running on port ${port}.`);
 });
 
 app.get("/entries", getCalendarEntries);
-connectToServer(() => {
-  const database = getDb();
-});
