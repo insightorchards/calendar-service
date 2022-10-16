138eafd26016079f9ca4ac7f7681433c9d200a42
Ellie Bahadori <elliedori@gmail.com>
2022-10-15 17:38:16 -0700

    Seed database with three entries and create README

--- api/controllers/calendarEntry.controller.ts
+++ api/controllers/calendarEntry.controller.ts
@@ -24,18 +24,40 @@ interface CalendarEntry {
   updatedAt: Date
 };
 
+const dayAfter = (date) => (new Date((date).valueOf() + 1000*3600*24))
+
 export const seedDatabaseWithEntry = async (req: Request, res: Response, next: NextFunction) => {
-  const date = new Date()
-  const entry: CalendarEntryInput = {
+  const today = new Date()
+  const entries: CalendarEntryInput[] = [
+    {
       eventId: "634b339218b3b892b312e5ca",
       creatorId: "424b339218b3b892b312e5cb",
       title: "Birthday party",
       description: "Let's celebrate Janie!",
       isAllDay: false,
-      startTimeUtc: date,
-      endTimeUtc: new Date((date).valueOf() + 1000*3600*24),
-  }
-  await db.collection('calendarEntries').insert(entry)
+      startTimeUtc: today,
+      endTimeUtc: dayAfter(today),
+    },
+    {
+      eventId: "634b339218b3b892b312e5ca",
+      creatorId: "424b339218b3b892b312e5cb",
+      title: "Dog walk",
+      description: "Time for Scottie walking",
+      isAllDay: false,
+      startTimeUtc: today,
+      endTimeUtc: dayAfter(today),
+    },
+    {
+      eventId: "634b339218b3b892b312e5ca",
+      creatorId: "424b339218b3b892b312e5cb",
+      title: "Hike with Bethany",
+      description: "Bethany wants to see Mt Tam!",
+      isAllDay: false,
+      startTimeUtc: today,
+      endTimeUtc: dayAfter(today),
+    }
+  ]
+  await db.collection('calendarEntries').insert(entries)
   res.sendStatus(201)
   return
 }
