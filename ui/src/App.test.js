ec4969454ed2c3dc77f5f5e4a2863556600c2e2f
Ashlynn223 <50315144+Ashlynn223@users.noreply.github.com>
2022-10-17 23:03:44 -0700

    add in test to ensure big calander ui is showing up and WIP on getting time stamps snapshot to show as one consistent time

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -1,14 +1,19 @@
-import { render } from '@testing-library/react';
+const MockDate = require('mockdate')
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  
   it('renders correctly', () => {
-    const app = render(App)
+    const NOW = '2022-05-25T00:25:55.444Z'
+    Date.now = jest.spyOn(Date, 'now').mockImplementation(() => 1530518207007)
+    const app = render(<App />)
     expect(app.asFragment()).toMatchSnapshot()
   })
 
-  xit('shows calander with event input fields', () => {})
+  it('shows calander with event input fields', async () => {
+    render(<App />)
+    expect(await screen.findByText("Month")).toBeVisible()
+  })
   
   describe("events", () => {
     xit('displays correct default values for event inputs on page load', () => {})
