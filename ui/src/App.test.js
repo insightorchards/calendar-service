8ab0792f9c0078c286c1dbbda53e9c30dc8bc4c4
Ashlynn223 <50315144+Ashlynn223@users.noreply.github.com>
2022-10-17 21:39:57 -0700

    add test empty test cases for the calender and event insertions

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  
+  it('renders correctly', () => {
+    const app = render(App)
+    expect(app.asFragment()).toMatchSnapshot()
+  })
+
+  xit('shows calander with event input fields', () => {})
+  
+  describe("events", () => {
+    xit('displays correct default values for event inputs on page load', () => {})
+    xit('displays event in ui when all inputs are provided valid values', () => {})
+    xit('errors when end date is before start date', () => {})
+    xit('errors when end time is before start time on the same day', () => {})
+  })
+})
