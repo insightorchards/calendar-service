7a3937c8aa608fe2e159104db86f13a707702cfa
Ashlynn223 <50315144+Ashlynn223@users.noreply.github.com>
2022-11-26 15:33:51 -0800

    add cypress test for asserting event details are shown correclty in modal view

--- e2e/cypress/e2e/spec.cy.js
+++ e2e/cypress/e2e/spec.cy.js
@@ -1,5 +1,17 @@
-describe("empty spec", () => {
-  it("passes", () => {
+describe("journey test", () => {
+  it("creates an event", () => {
     cy.visit("http://localhost:3000");
+    cy.contains("label", "Title").click().type("Hello");
+    cy.contains("label", "Start Date").click().type("2022-11-26");
+    cy.contains("label", "End Date").click().type("2022-11-27");
+    cy.contains("label", "Start Time").click().type("04:35");
+    cy.contains("label", "End Time").click().type("06:45");
+    cy.contains("button", "Create Event").click();
+    cy.contains("Hello").click();
+    cy.contains("Event title: Hello").should("be.visible");
+    cy.contains("Description: default entry description").should("be.visible");
+    cy.contains("Start: Saturday, November 26 04:35 AM").should("be.visible");
+    cy.contains("End: Sunday, November 27 06:45 AM").should("be.visible");
+    cy.contains("button", "Delete").click();
   });
 });
