0a51dcbe34a6042b7af6c59090e13cf401b77cb8
sofiapaganin <paganin@berkeley.edu>
2022-11-30 21:48:57 -0800

    add basic EventForm test

--- ui/src/EventForm.test.tsx
+++ ui/src/EventForm.test.tsx
@@ -1,16 +1,76 @@
 import { act, render, screen, waitFor } from "@testing-library/react";
 import React from "react";
+
 import userEvent from "@testing-library/user-event";
-import App from "./App";
 import { createEntry, deleteEntry, getEntries, getEntry } from "./hooks";
-jest.mock("./hooks");
+import { formatDate, padNumberWith0Zero } from "./lib";
 import EventForm from "./EventForm";
 
-const mockCreateEntry = createEntry as jest.MockedFunction<typeof createEntry>;
-const mockGetEntries = getEntries as jest.MockedFunction<typeof getEntries>;
-const mockGetEntry = getEntry as jest.MockedFunction<typeof getEntry>;
-const mockDeleteEntry = deleteEntry as jest.MockedFunction<typeof deleteEntry>;
-
 describe("EventForm", () => {
+  beforeAll(() => {
+    jest.useFakeTimers("modern" as FakeTimersConfig);
+    const date = new Date("2022-02-15T04:00");
+    jest.setSystemTime(date);
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  it("displays initial values", () => {
+    const currentHour: number = new Date().getHours();
+    const currentMinute: number = new Date().getMinutes();
+
+    render(
+      <EventForm
+        initialStartDate={formatDate(new Date())}
+        initialEndDate={formatDate(new Date())}
+        initialStartTime={`${padNumberWith0Zero(
+          currentHour,
+        )}:${padNumberWith0Zero(currentMinute)}`}
+        initialEndTime={`${padNumberWith0Zero(
+          currentHour + 1,
+        )}:${padNumberWith0Zero(currentMinute)}`}
+        initialTitle="Mary's Chicken Feast"
+        initialDescription="A time to remember and appreciate chicken nuggets and more"
+        onSave={() => {}}
+        isCreate={true}
+      />,
+    );
+
+    expect(screen.getByLabelText("Start Date")).toHaveValue("2022-02-15");
+    expect(screen.getByLabelText("End Date")).toHaveValue("2022-02-15");
+    expect(screen.getByLabelText("Start Time")).toHaveValue("04:00");
+    expect(screen.getByLabelText("End Time")).toHaveValue("05:00");
+    expect(screen.getByLabelText("Title")).toHaveValue("Mary's Chicken Feast");
+    expect(screen.getByLabelText("Description")).toHaveValue(
+      "A time to remember and appreciate chicken nuggets and more",
+    );
+    expect(screen.getByRole("button")).toHaveAccessibleName("Create Event");
+  });
+
+  it("displays `Save` button when `isCreate` is false", () => {
+    const currentHour: number = new Date().getHours();
+    const currentMinute: number = new Date().getMinutes();
+
+    render(
+      <EventForm
+        initialStartDate={formatDate(new Date())}
+        initialEndDate={formatDate(new Date())}
+        initialStartTime={`${padNumberWith0Zero(
+          currentHour,
+        )}:${padNumberWith0Zero(currentMinute)}`}
+        initialEndTime={`${padNumberWith0Zero(
+          currentHour + 1,
+        )}:${padNumberWith0Zero(currentMinute)}`}
+        initialTitle="Arty party"
+        initialDescription="A time to remember and appreciate classic art and more"
+        onSave={() => {}}
+        isCreate={false}
+      />,
+    );
+
+    expect(screen.getByRole("button")).toHaveAccessibleName("Save");
+  });
   it("mutes out time sections when all day is selected", () => {});
 });
