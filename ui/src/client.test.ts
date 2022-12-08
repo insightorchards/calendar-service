fe34f24183daf61891e14303eb60b9a3f36624ce
sofiapaganin <paganin@berkeley.edu>
2022-12-07 22:22:22 -0800

    add test for createEntry

--- ui/src/client.test.ts
+++ ui/src/client.test.ts
@@ -1,4 +1,4 @@
-const { getEntries, getEntry } = require("./client");
+const { getEntries, getEntry, createEntry } = require("./client");
 
 describe("client functions", () => {
   describe("getEntries", () => {
@@ -68,6 +68,55 @@ describe("client functions", () => {
         .mockResolvedValue(mockResponse);
 
       const result = await getEntry("638d815856e5c70955565b7e");
+      expect(fetchSpy).toHaveBeenCalled();
+      expect(result).toEqual({
+        _id: "638d815856e5c70955565b7e",
+        eventId: "5678",
+        creatorId: "1234",
+        title: "Ange's Bat Mitzvah",
+        description: "Ange is turning 13!",
+        allDay: false,
+        startTimeUtc: "2024-06-06T01:07:00.000Z",
+        endTimeUtc: "2024-06-06T05:07:00.000Z",
+        createdAt: "2022-12-05T05:27:52.212Z",
+        updatedAt: "2022-12-05T05:27:52.212Z",
+        __v: 0,
+      });
+    });
+  });
+
+  describe("createEntry", () => {
+    it("succeeds", async () => {
+      const mockResponse = {
+        status: 200,
+        json: () => {
+          return {
+            _id: "638d815856e5c70955565b7e",
+            eventId: "5678",
+            creatorId: "1234",
+            title: "Ange's Bat Mitzvah",
+            description: "Ange is turning 13!",
+            allDay: false,
+            startTimeUtc: "2024-06-06T01:07:00.000Z",
+            endTimeUtc: "2024-06-06T05:07:00.000Z",
+            createdAt: "2022-12-05T05:27:52.212Z",
+            updatedAt: "2022-12-05T05:27:52.212Z",
+            __v: 0,
+          } as any;
+        },
+      } as Response;
+
+      const fetchSpy = jest
+        .spyOn(window, "fetch")
+        .mockResolvedValue(mockResponse);
+
+      const result = await createEntry({
+        title: "Ange's Bat Mitzvah",
+        description: "Ange is turning 13!",
+        startTimeUtc: new Date("2024-06-06T01:07:00.000Z"),
+        endTimeUtc: new Date("2024-06-06T01:07:00.000Z"),
+        allDay: true,
+      });
       expect(fetchSpy).toHaveBeenCalled();
       expect(result).toEqual({
         _id: "638d815856e5c70955565b7e",
