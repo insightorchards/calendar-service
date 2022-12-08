cbbfafa5a914d79f65f993a724e517e529f30d4b
sofiapaganin <paganin@berkeley.edu>
2022-12-07 22:25:13 -0800

    add test for updateEntry

--- ui/src/client.test.ts
+++ ui/src/client.test.ts
@@ -1,4 +1,4 @@
-const { getEntries, getEntry, createEntry } = require("./client");
+import { getEntries, getEntry, createEntry, updateEntry } from "./client";
 
 describe("client functions", () => {
   describe("getEntries", () => {
@@ -124,6 +124,59 @@ describe("client functions", () => {
         creatorId: "1234",
         title: "Ange's Bat Mitzvah",
         description: "Ange is turning 13!",
+        allDay: false,
+        startTimeUtc: "2024-06-06T01:07:00.000Z",
+        endTimeUtc: "2024-06-06T05:07:00.000Z",
+        createdAt: "2022-12-05T05:27:52.212Z",
+        updatedAt: "2022-12-05T05:27:52.212Z",
+        __v: 0,
+      });
+    });
+  });
+
+  describe("updateEntry", () => {
+    it("succeeds", async () => {
+      const mockResponse = {
+        status: 200,
+        json: () => {
+          return {
+            _id: "638d815856e5c70955565b7e",
+            eventId: "5678",
+            creatorId: "1234",
+            title: "Barbies's Bat Mitzvah",
+            description: "Barbie is turning 13!",
+            allDay: false,
+            startTimeUtc: "2024-06-06T01:07:00.000Z",
+            endTimeUtc: "2024-06-06T05:07:00.000Z",
+            createdAt: "2022-12-05T05:27:52.212Z",
+            updatedAt: "2022-12-05T05:27:52.212Z",
+            __v: 0,
+          } as any;
+        },
+      } as Response;
+
+      const fetchSpy = jest
+        .spyOn(window, "fetch")
+        .mockResolvedValue(mockResponse);
+
+      const result = await updateEntry("638d815856e5c70955565b7e", {
+        title: "Barbies's Bat Mitzvah",
+        description: "Barbie is turning 13!",
+        startTimeUtc: new Date("2024-06-06T01:07:00.000Z"),
+        endTimeUtc: new Date("2024-06-06T01:07:00.000Z"),
+        allDay: true,
+      });
+      expect(fetchSpy).toHaveBeenCalled();
+      expect(fetchSpy.mock.calls[0][1]).toEqual(
+        expect.objectContaining({ method: "PATCH" }),
+      );
+
+      expect(result).toEqual({
+        _id: "638d815856e5c70955565b7e",
+        eventId: "5678",
+        creatorId: "1234",
+        title: "Barbies's Bat Mitzvah",
+        description: "Barbie is turning 13!",
         allDay: false,
         startTimeUtc: "2024-06-06T01:07:00.000Z",
         endTimeUtc: "2024-06-06T05:07:00.000Z",
