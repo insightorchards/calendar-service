cf42bae749895064e83c6dacac0985e41e8f6a0a
Ellie Bahadori <elliedori@gmail.com>
2023-01-29 15:00:19 -0800

    Add styling to counter

--- packages/calendar-ui/src/counter.tsx
+++ packages/calendar-ui/src/counter.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
+import s from "./counter.module.css";
 
 export function Counter() {
   const [count, setCount] = React.useState(0);
   return (
     <>
-      <p>You clicked {count} times</p>
+      <p className={s.red}>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
     </>
   );
