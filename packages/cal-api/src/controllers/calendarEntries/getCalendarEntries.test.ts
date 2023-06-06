fc8e3071466f146354e594882df438d3fb8b14db
Charles Magic Wooper <codepath2019@gmail.com>
2023-06-05 18:44:15 -0700

    add ability to search calendar entries within a range and update build step to not compile tests

--- packages/cal-api/src/controllers/calendarEntries/getCalendarEntries.test.ts
+++ packages/cal-api/src/controllers/calendarEntries/getCalendarEntries.test.ts
@@ -73,3 +73,85 @@ describe("GET /calendars/:id/entries", () => {
     expect(entries.length).toEqual(2);
   });
 });
+
+describe("GET /calendars/:id/entries?start=<start-time>&end=<end-time>", () => {
+  beforeEach(async () => {
+    const createCalendarResponse = await supertest(app)
+      .post("/calendars")
+      .send({
+        eventId: "id of cosmic party event",
+        creatorId: "id of creator of cosmic party",
+        title: "Parties!",
+      })
+      .expect(201);
+
+    const calendar = JSON.parse(createCalendarResponse.text);
+
+    await supertest(app)
+      .post("/calendars/:id/entries")
+      .send({
+        calendarId: calendar._id,
+        creatorId: "id of creator of cosmic party",
+        title: "Dance party at the Guggenheim",
+        allDay: true,
+        recurring: false,
+        startTimeUtc: new Date("2023-05-30T07:00:00.000Z"),
+        endTimeUtc: new Date("2023-05-31T06:59:59.999Z"),
+      })
+      .expect(201);
+
+    await supertest(app)
+      .post("/calendars/:id/entries")
+      .send({
+        calendarId: calendar._id,
+        creatorId: "id of creator of cosmic party",
+        title: "Dinner party at Le Louvre",
+        allDay: true,
+        recurring: false,
+        startTimeUtc: new Date("2023-06-15T07:00:00.000Z"),
+        endTimeUtc: new Date("2023-06-16T06:59:59.999Z"),
+      })
+      .expect(201);
+
+    await supertest(app)
+      .post("/calendars/:id/entries")
+      .send({
+        calendarId: calendar._id,
+        creatorId: "id of creator of cosmic party",
+        title: "Desert party atop the Eifel",
+        allDay: true,
+        recurring: false,
+        startTimeUtc: new Date("2023-06-16T06:59:59.999Z"),
+        endTimeUtc: new Date("2023-06-16T07:00:00.000Z"),
+      })
+      .expect(201);
+  });
+
+  it("gets all calendar entries that starts and ends within the range", async () => {
+    const calendar = await Calendar.findOne({}).sort({
+      $natural: -1,
+    });
+
+    const response = await supertest(app)
+      .get(
+        `/calendars/${calendar._id}/entries?start=2023-05-30T07:00:00.000Z&end=2023-06-16T06:59:59.999Z`
+      )
+      .send()
+      .expect(200);
+
+    const entries = JSON.parse(response.text);
+    expect(entries.length).toEqual(2);
+
+    expect(entries[0]).toEqual(
+      expect.objectContaining({
+        title: "Dance party at the Guggenheim",
+      })
+    );
+
+    expect(entries[1]).toEqual(
+      expect.objectContaining({
+        title: "Dinner party at Le Louvre",
+      })
+    );
+  });
+});
--- packages/cal-api/src/controllers/calendarEntries/getCalendarEntries.ts
+++ packages/cal-api/src/controllers/calendarEntries/getCalendarEntries.ts
@@ -8,9 +8,19 @@ const getCalendarEntries = async (
 ) => {
   try {
     const { id: calendarId } = req.params;
-    const entries = await CalendarEntry.find()
-      .where("calendarId")
-      .equals(calendarId);
+    const { start, end } = req.query;
+
+    const findSpec: {
+      calendarId: string;
+      startTimeUtc?: object;
+      endTimeUtc?: object;
+    } = { calendarId };
+
+    if (start) findSpec.startTimeUtc = { $gte: start };
+    if (end) findSpec.endTimeUtc = { $lte: end };
+
+    const entries = await CalendarEntry.find(findSpec);
+
     res.status(200).json(entries);
   } catch (err) {
     res.status(400);
